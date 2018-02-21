c03ea7512977a6099e1360be157ef9c6a2c89e79
AdelLarbi <larbi.adel@yahoo.fr>
2018-02-21 16:07:46 +0100

    mysql connection with simple request

--- routes/test.js
+++ routes/test.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET another page */
 router.get('/', function(req, res, next) {
     res.render('test', { title: 'SEGULA' });
 });
 
 module.exports = router;
+
+/* Connect to data base and fetch data */
+var db = require('../database/config');
+db.connect(function(err) {
+    if (!err) {
+        console.log('Connected to the database.');
+        showPersons();
+
+    } else {
+        throw err;
+    }
+});
+
+/**
+ *  --------------
+ *  | Persons    |
+ *  --------------
+ *  | id         |
+ *  | firstName  |
+ *  | secondName |
+ *  --------------
+ */
+var showPersons = function () {
+    var request = 'SELECT * FROM Persons;';
+    db.query(request)
+        .on('result', function (result) {
+            console.log(result);
+            console.log('id : ' + result.id);
+            console.log('firstName : ' + result.firstName);
+            console.log('secondName : ' + result.secondName);
+        })
+        .on('error', function (err) {
+            console.log('error : ' + err);
+        });
+};
